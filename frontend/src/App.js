c0e95271aac27ac0469a58ddde6771e789921291
db1962 <baird.derek@gmail.com>
2022-04-19 11:03:52 +1000

    Frontend commit 1st

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ProposalData from './components/proposalData';
+function App() {
+  return (
+    <div className="App">
+     
+     
+                 <h1>Institutional Pricing Tool 5.0.1   User123</h1>
+        
+     <ProposalData />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/proposalData.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import DataGrid, { SelectColumn } from 'react-data-grid'
+
+
+// function getRowId(row) {
+//     return row.Id;
+// }
+
+function onRowChanged() {
+
+}
+
+
+export default function ProposalData() {
+    
+    const [rows, setRows] = useState([])
+   
+    const getData = () => {
+        fetch('https://localhost:7211/api/Proposals')
+            .then((res) => res.json())
+            .then((res) => {
+
+                setRows(res)
+            })
+    }
+
+    useEffect(()=> {
+        getData();
+    }, [])
+
+    const columns = [
+        {
+            key: 'expanded',
+            name: '',
+            minWidth: 30,
+            width: 30,
+            
+        },
+        {key:'id',name:'Id'},
+        {key:'proposalName', name: 'Proposal Name'},
+        {key:'customerGrpName', name: 'Customer Name'},
+        {key:'date', name: 'Date Modified'},
+        {key:'description', name: 'Description'},
+        {key:'proposalStatus', name: 'Status'}
+    ]
+
+  
+
+    return (<DataGrid columns={columns} rows={rows}  
+    onRowsChange={onRowChanged}
+    />)
+   
+}
